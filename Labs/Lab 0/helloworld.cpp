1f851454ebe4d56f8feabb5168040051839a2e96
nzoladkiewicz <94705730+nzoladkiewicz@users.noreply.github.com>
2023-09-18 14:22:30 -0400

    Add files via upload

---
+++ Labs/Lab 0/helloworld.cpp
@@ -0,0 +1,17 @@
+/*******************************************************************************
+* Filename: helloworld.cpp
+* Author : Natalie Zoladkiewicz
+* Version : 1.0
+* Date : 09/05/2023
+* Description: Basic Hello World program.
+* Pledge : I pledge my honor that I have abided by the Stevens Honor System.
+****************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Hello world!" << endl;
+    return 0;
+}
