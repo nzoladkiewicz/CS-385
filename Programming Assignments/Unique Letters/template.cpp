c69c7921ca7bbd3c3e3347fac5a93d6933f15bc0
nzoladkiewicz <94705730+nzoladkiewicz@users.noreply.github.com>
2024-02-17 00:20:22 -0500

    Create template.cpp

---
+++ Programming Assignments/Unique Letters/template.cpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Name        : unique.cpp
+ * Author      : 
+ * Date        : 
+ * Description : Determining uniqueness of chars with int as bit vector.
+ * Pledge      :
+ ******************************************************************************/
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+bool is_all_lowercase(const string &s) {
+    // TODO: returns true if all characters in string are lowercase
+    // letters in the English alphabet; false otherwise.
+}
+
+bool all_unique_letters(const string &s) {
+    // TODO: returns true if all letters in string are unique, that is
+    // no duplicates are found; false otherwise.
+
+    // You MUST use only single unsigned integers and work with bitwise
+    // and bitshifting operators only.  Using any other kind of solution
+    // will automatically result in a grade of ZERO for the whole assignment.
+}
+
+int main(int argc, char * const argv[]) {
+    // TODO: reads and parses command line arguments.
+    // Calls other functions to produce correct output.
+}
